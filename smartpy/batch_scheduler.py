95a92ecf2dac077b277b435b0b04906f62e90e12
Adam Salvail <Adam.Salvail@USherbrooke.ca>
2015-07-01 14:20:42 -0400

    Modified ``Minibatches`` to support minibatch resizing.

--- smartpy/batch_scheduler.py
+++ smartpy/batch_scheduler.py
@@ -22,14 +22,23 @@ class BatchScheduler(object):
 class MiniBatchScheduler(BatchScheduler):
     def __init__(self, dataset, batch_size):
         super(MiniBatchScheduler, self).__init__(dataset)
+        self._shared_batch_size = theano.shared(np.array(0, dtype='i4'))
         self.batch_size = batch_size
-        self.nb_updates_per_epoch = int(np.ceil(len(self.dataset)/self.batch_size))
         self.shared_batch_count = theano.shared(np.array(0, dtype='i4'))
 
     @property
+    def batch_size(self):
+        return self._shared_batch_size.get_value()
+
+    @batch_size.setter
+    def batch_size(self, value):
+        self._shared_batch_size.set_value(np.array(value, dtype='i4'))
+        self.nb_updates_per_epoch = int(np.ceil(len(self.dataset)/self.batch_size))
+
+    @property
     def givens(self):
-        start = self.shared_batch_count * self.batch_size
-        end = (self.shared_batch_count + 1) * self.batch_size
+        start = self.shared_batch_count * self._shared_batch_size
+        end = (self.shared_batch_count + 1) * self._shared_batch_size
 
         if self.dataset.targets is not None:
             return {self.dataset.symb_inputs: self.dataset.inputs[start:end],
