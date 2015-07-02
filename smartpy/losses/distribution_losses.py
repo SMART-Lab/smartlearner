fca042cc862fb08e133dd1e2a2a455ca329270dc
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2015-07-02 13:45:37 -0400

    Removed commented line

--- smartpy/losses/distribution_losses.py
+++ smartpy/losses/distribution_losses.py
@@ -6,4 +6,3 @@ import theano.tensor as T
 class BinaryCrossEntropy(Loss):
     def _loss_function(self, model_output):
         return T.mean(T.nnet.binary_crossentropy(model_output, self.target))
-        #return T.mean(T.sum(T.nnet.softplus(-self.target.T * model_output.T + (1 - self.target.T) * model_output.T), axis=0))
