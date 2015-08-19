afbd75fc36086e60d5c840fbe9d450a4dde4d964
Adam Salvail <Adam.Salvail@USherbrooke.ca>
2015-08-19 15:20:18 -0400

    Adds Reconstruction error view

--- smartlearner/views.py
+++ smartlearner/views.py
@@ -51,3 +51,39 @@ class ClassificationError(View):
     @property
     def stderror(self):
         return ItemGetter(self, attribute=1)
+
+
+class ReconstructionError(View):
+    def __init__(self, predict_fct, dataset):
+        super().__init__()
+
+        batch_size = 1024  # Internal buffer
+        self.nb_batches = int(np.ceil(len(dataset) / batch_size))
+
+        input = dataset.symb_inputs
+        target = dataset.symb_targets
+        reconstruction_errors = T.sqr(predict_fct(input) - target).mean()
+
+        no_batch = T.iscalar('no_batch')
+        givens = {input: dataset.inputs[no_batch * batch_size:(no_batch + 1) * batch_size],
+                  target: dataset.targets[no_batch * batch_size:(no_batch + 1) * batch_size]}
+        self.compute_reconstruction_error = theano.function([no_batch],
+                                                            reconstruction_errors,
+                                                            givens=givens,
+                                                            name="compute_reconstruction_error_" + dataset.name)
+
+    def update(self, status):
+        reconstruction_errors = []
+        for i in range(self.nb_batches):
+            reconstruction_errors.append(self.compute_reconstruction_error(i))
+
+        reconstruction_errors = np.array(reconstruction_errors)
+        return reconstruction_errors.mean(), reconstruction_errors.std(ddof=1) / np.sqrt(len(reconstruction_errors))
+
+    @property
+    def mean(self):
+        return ItemGetter(self, attribute=0)
+
+    @property
+    def stderror(self):
+        return ItemGetter(self, attribute=1)
